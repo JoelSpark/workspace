96b55e82fff0d64c62a66acbc96265905a3897d6
Joel Spark <jspark@jspark-aspire.(none)>
2014-03-12 17:11:05 -0700

    added size limited file sink heir block

--- rx_local_debug.py
+++ rx_local_debug.py
@@ -2,13 +2,12 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Rx Local Debug
-# Generated: Thu Nov 21 21:13:29 2013
+# Generated: Wed Mar 12 14:00:11 2014
 ##################################################
 
 from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
 from gnuradio import uhd
 from gnuradio import wxgui
@@ -19,10 +18,6 @@ from gnuradio.wxgui import fftsink2
 from gnuradio.wxgui import forms
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
-import SimpleXMLRPCServer
-import pmt
-import satisfi
-import threading
 import time
 import wx
 
@@ -30,18 +25,15 @@ class rx_local_debug(grc_wxgui.top_block_gui):
 
     def __init__(self):
         grc_wxgui.top_block_gui.__init__(self, title="Rx Local Debug")
-        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
-        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
         # Variables
         ##################################################
         self.usrp_gain = usrp_gain = 5
         self.tracking = tracking = 0
-        self.sat_name = sat_name = "PRISM"
-        self.samp_rate = samp_rate = 400e3
+        self.samp_rate = samp_rate = 1e6
         self.offset = offset = 0
-        self.freq = freq = 437e6
+        self.freq = freq = 401.829e6
         self.doppler_shift = doppler_shift = 0
         self.doppler_enable = doppler_enable = False
         self.adjust = adjust = 0
@@ -118,9 +110,6 @@ class rx_local_debug(grc_wxgui.top_block_gui):
         	converter=forms.float_converter(),
         )
         self.Add(self._LO_offset_text_box)
-        self.xmlrpc_server_0 = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost", 8808), allow_none=True)
-        self.xmlrpc_server_0.register_instance(self)
-        threading.Thread(target=self.xmlrpc_server_0.serve_forever).start()
         self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
         	self.nb0.GetPage(0).GetWin(),
         	baseband_freq=0,
@@ -128,8 +117,8 @@ class rx_local_debug(grc_wxgui.top_block_gui):
         	y_divs=10,
         	ref_level=0,
         	ref_scale=2.0,
-        	sample_rate=samp_rate/10,
-        	fft_size=2048,
+        	sample_rate=samp_rate/1,
+        	fft_size=1024,
         	fft_rate=15,
         	average=False,
         	avg_alpha=None,
@@ -138,7 +127,7 @@ class rx_local_debug(grc_wxgui.top_block_gui):
         )
         self.nb0.GetPage(0).Add(self.wxgui_fftsink2_0.win)
         self.uhd_usrp_source_0 = uhd.usrp_source(
-        	device_addr="addr=192.168.10.2",
+        	device_addr="addr=192.168.10.3",
         	stream_args=uhd.stream_args(
         		cpu_format="fc32",
         		channels=range(1),
@@ -156,32 +145,19 @@ class rx_local_debug(grc_wxgui.top_block_gui):
         	converter=forms.float_converter(),
         )
         self.Add(self._tracking_static_text)
-        self.satisfi_predict_query_0 = satisfi.predict_query(predict_ip="127.0.0.1",predict_port=1210,sat_name=sat_name,freq=freq,xml_port=8808,xml_ip="127.0.0.1")
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=1,
-                decimation=10,
-                taps=None,
-                fractional_bw=None,
-        )
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("TEST"), 1000)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, "/home/jspark/AS1/1")
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, "/home/jspark/sample_interference.iq", False)
         self.blocks_file_sink_0.set_unbuffered(False)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate/10, analog.GR_COS_WAVE, -adjust-(doppler_shift*doppler_enable), 1, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate/1, analog.GR_COS_WAVE, -adjust-(doppler_shift*doppler_enable), 1, 0)
 
         ##################################################
         # Connections
         ##################################################
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.uhd_usrp_source_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.wxgui_fftsink2_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_multiply_xx_0, 0))
 
-        ##################################################
-        # Asynch Message Connections
-        ##################################################
-        self.msg_connect(self.blocks_message_strobe_0, "strobe", self.satisfi_predict_query_0, "trig")
 
 # QT sink close method reimplementation
 
@@ -200,21 +176,14 @@ class rx_local_debug(grc_wxgui.top_block_gui):
         self.tracking = tracking
         self._tracking_static_text.set_value(self.tracking)
 
-    def get_sat_name(self):
-        return self.sat_name
-
-    def set_sat_name(self, sat_name):
-        self.sat_name = sat_name
-        self.satisfi_predict_query_0.set_name(self.sat_name)
-
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate/10)
-        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate/10)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate/1)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate/1)
 
     def get_offset(self):
         return self.offset
@@ -229,7 +198,6 @@ class rx_local_debug(grc_wxgui.top_block_gui):
 
     def set_freq(self, freq):
         self.freq = freq
-        self.satisfi_predict_query_0.set_freq(self.freq)
         self._freq_text_box.set_value(self.freq)
         self.uhd_usrp_source_0.set_center_freq(uhd.tune_request(self.freq+self.offset,self.LO_offset), 0)
 
@@ -253,9 +221,9 @@ class rx_local_debug(grc_wxgui.top_block_gui):
 
     def set_adjust(self, adjust):
         self.adjust = adjust
-        self.analog_sig_source_x_0.set_frequency(-self.adjust-(self.doppler_shift*self.doppler_enable))
         self._adjust_slider.set_value(self.adjust)
         self._adjust_text_box.set_value(self.adjust)
+        self.analog_sig_source_x_0.set_frequency(-self.adjust-(self.doppler_shift*self.doppler_enable))
 
     def get_LO_offset(self):
         return self.LO_offset
@@ -266,6 +234,14 @@ class rx_local_debug(grc_wxgui.top_block_gui):
         self.uhd_usrp_source_0.set_center_freq(uhd.tune_request(self.freq+self.offset,self.LO_offset), 0)
 
 if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
     (options, args) = parser.parse_args()
     tb = rx_local_debug()
